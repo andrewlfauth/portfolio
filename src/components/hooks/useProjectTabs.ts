8aea20b45e2b6d29f30118c23632a97f8a385112
andrewlfauth <andrewlfauth@gmail.com>
2023-01-31 15:03:48 -0800

    tab typing

--- src/components/hooks/useProjectTabs.ts
+++ src/components/hooks/useProjectTabs.ts
@@ -1,7 +1,9 @@
 import { useState, useRef } from 'preact/hooks'
 
+type tabType = 'screenshots' | 'features' | 'about'
+
 export default function useProjectTabs() {
-  const [tab, setTab] = useState('screenshots')
+  const [tab, setTab] = useState<tabType>('screenshots')
   const tabsRef = useRef<HTMLDivElement>(null)
 
   return { tab, setTab, tabsRef }
