3b488cd4f1bf60f0a17dd8476a674afe5d17b165
andrewlfauth <andrewlfauth@gmail.com>
2023-02-09 13:08:51 -0800

    update active link style

--- src/components/Header/NavLink.tsx
+++ src/components/Header/NavLink.tsx
@@ -10,14 +10,14 @@ interface Props {
 
 function NavLink({ href, text, handleClick, section, activeSection }: Props) {
   const activeClass = clsx(
-    'after:scale-x-100 text-mat-red catppuccin:text-cat-blue nightowl:text-nightowl-orange'
+    'after:scale-x-100 text-mat-red catppuccin:text-cat-blue nightowl:text-nightowl-orange after:scale-x-100'
   )
 
   return (
     <li
       onClick={handleClick && handleClick}
-      className={`hover:text-mat-red hover:catppuccin:text-cat-blue hover:nightowl:text-nightowl-orange after:bg-mat-red catppuccin:after:bg-cat-blue nightowl:after:bg-nightowl-orange after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:scale-x-0 hover:after:scale-x-100 after:duration-200 after:origin-bottom-left relative duration-200 ${
-        activeSection == section ? activeClass : 'text-type '
+      className={`hover:text-mat-red hover:catppuccin:text-cat-blue hover:nightowl:text-nightowl-orange after:bg-mat-red catppuccin:after:bg-cat-blue nightowl:after:bg-nightowl-orange after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full hover:after:scale-x-100 after:duration-200 after:origin-bottom-left relative duration-200 ${
+        activeSection == section ? activeClass : 'text-type after:scale-x-0'
       }`}
     >
       <a href={href}>{text}</a>
