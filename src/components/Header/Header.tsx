53f485d6c58a087a661486ff87f972c390c95c88
andrewlfauth <andrewlfauth@gmail.com>
2023-02-09 14:42:44 -0800

    update Header padding and animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,38 @@
-import { useEffect, useRef } from "preact/hooks"
-import { gsap } from "../../utils/gsap"
-import { settings } from "../../stores/settings"
-import CursorToggle from "./CursorToggle"
-import github from "../../assets/github.svg"
-import linkedin from "../../assets/linkedin.svg"
-import resume from "../../assets/resume.svg"
-import Nav from "./Nav"
+import { useEffect, useRef } from 'preact/hooks'
+import { gsap } from '../../utils/gsap'
+import { settings } from '../../stores/settings'
+import CursorToggle from './CursorToggle'
+import github from '../../assets/github.svg'
+import linkedin from '../../assets/linkedin.svg'
+import resume from '../../assets/resume.svg'
+import Nav from './Nav'
 
 function Sidebar() {
   const btnsRef = useRef<HTMLDivElement>(null)
 
-  const openContactForm = () => settings.setKey("showContactForm", true)
+  const openContactForm = () => settings.setKey('showContactForm', true)
 
   useEffect(() => {
     gsap.to(btnsRef.current, {
       scrollTrigger: {
         trigger: btnsRef.current,
-        start: "bottom -370",
-        scrub: true,
+        start: 'bottom -320',
+        end: 'bottom -332',
+        scrub: 1,
       },
-      opacity: 100,
-      pointerEvents: "auto",
+      opacity: 1,
+      x: 0,
+      pointerEvents: 'auto',
     })
   }, [])
 
   return (
     <>
-      <div className='flex fixed left-0 top-0 z-50 space-x-2 p-2 md:p-4'>
+      <div className='flex fixed left-0 top-0 z-50 space-x-2 p-4'>
         <CursorToggle />
         <div
           ref={btnsRef}
-          className='flex space-x-2 opacity-0 pointer-events-none'
+          className='flex space-x-2 opacity-0 pointer-events-none translate-x-10'
         >
           <button
             class='h-10 w-20 md:w-[156px] md:h-[52px] rounded-lg border-2 border-primary text-mat-red catppuccin:text-cat-pink nightowl:text-nightowl-orange text-sm md:text-2xl font-medium font-redhat flex items-center justify-center select-none hover:scale-[1.02] bg-primary'
