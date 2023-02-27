35ae8120fe33d9d91b25fc5da7b3f5406085b18a
andrewlfauth <andrewlfauth@gmail.com>
2023-02-27 13:24:43 -0800

    useMemo for screenshot images

--- src/components/Screentime/ScreenShots.tsx
+++ src/components/Screentime/ScreenShots.tsx
@@ -1,56 +1,63 @@
 import ImageCycler from "../Projects/ImageCycler"
+import { useMemo } from "preact/hooks"
 
 function ScreenShots() {
-  let mobile = [
-    {
-      id: 0,
-      src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1670971602/portfolio/e2t34y1bk9lhefemyigv.png",
-      alt: "screen time mobile landing page",
-    },
-    {
-      id: 1,
-      src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672434800/portfolio/je6ut8lesoiklzwkecdn.png",
-      alt: "screen time mobile dashboard page",
-    },
-    {
-      id: 2,
-      src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672432118/portfolio/jd83ur326hdhqkmfqvxy.png",
-      alt: "screen time mobile plans page",
-    },
-    {
-      id: 3,
-      src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672432267/portfolio/vttmta3aq7hpoevgmces.png",
-      alt: "screen time mobile shows page",
-    },
-    {
-      id: 4,
-      src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672432343/portfolio/h0oza8t8bp2gkfszl7xb.png",
-      alt: "screen time mobile favorites page",
-    },
-  ]
+  let mobile = useMemo(
+    () => [
+      {
+        id: 0,
+        src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1670971602/portfolio/e2t34y1bk9lhefemyigv.png",
+        alt: "screen time mobile landing page",
+      },
+      {
+        id: 1,
+        src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672434800/portfolio/je6ut8lesoiklzwkecdn.png",
+        alt: "screen time mobile dashboard page",
+      },
+      {
+        id: 2,
+        src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672432118/portfolio/jd83ur326hdhqkmfqvxy.png",
+        alt: "screen time mobile plans page",
+      },
+      {
+        id: 3,
+        src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672432267/portfolio/vttmta3aq7hpoevgmces.png",
+        alt: "screen time mobile shows page",
+      },
+      {
+        id: 4,
+        src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672432343/portfolio/h0oza8t8bp2gkfszl7xb.png",
+        alt: "screen time mobile favorites page",
+      },
+    ],
+    []
+  )
 
-  let desktop = [
-    {
-      id: 0,
-      src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672431363/portfolio/yqjfkpfsmbptpht11ekm.png",
-      alt: "screen time dashboard page",
-    },
-    {
-      id: 1,
-      src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672431443/portfolio/bj02t91tyok7usi5cqso.png",
-      alt: "screen time plans page",
-    },
-    {
-      id: 2,
-      src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672431607/portfolio/dc2oyl1rf2zgytlb03u5.png",
-      alt: "screen time shows page",
-    },
-    {
-      id: 3,
-      src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672431682/portfolio/tqryfa5iyi77csznmemd.png",
-      alt: "screen time favorites page",
-    },
-  ]
+  let desktop = useMemo(
+    () => [
+      {
+        id: 0,
+        src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672431363/portfolio/yqjfkpfsmbptpht11ekm.png",
+        alt: "screen time dashboard page",
+      },
+      {
+        id: 1,
+        src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672431443/portfolio/bj02t91tyok7usi5cqso.png",
+        alt: "screen time plans page",
+      },
+      {
+        id: 2,
+        src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672431607/portfolio/dc2oyl1rf2zgytlb03u5.png",
+        alt: "screen time shows page",
+      },
+      {
+        id: 3,
+        src: "https://res.cloudinary.com/dpnkrz8c8/image/upload/v1672431682/portfolio/tqryfa5iyi77csznmemd.png",
+        alt: "screen time favorites page",
+      },
+    ],
+    []
+  )
 
   return (
     <div className='flex lg:space-x-4 mb-6 lg:mb-0'>
