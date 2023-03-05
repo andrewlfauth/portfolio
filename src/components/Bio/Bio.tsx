1b5a50feb863b73c2dffd98ed26537a024042b37
andrewlfauth <andrewlfauth@gmail.com>
2023-03-04 17:44:13 -0800

    update bio text

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -6,21 +6,21 @@ function Bio() {
   const [showMore, setShowMore] = useState(false)
 
   return (
-    <div id='bio' className='pt-60 px-4 sm:px-6'>
-      <h2 className='font-redhat text-4xl md:text-5xl text-title text-center mb-14 md:mb-24'>
+    <div id="bio" className="pt-60 px-4 sm:px-6">
+      <h2 className="font-redhat text-4xl md:text-5xl text-title text-center mb-14 md:mb-24">
         Bio
       </h2>
-      <div className='text-type md:text-2xl lg:text-3xl font-medium max-w-4xl mx-auto text-center'>
-        <p className='mb-6'>
-          Became interested in coding while working as a SEO
+      <div className="text-type md:text-2xl lg:text-3xl font-medium max-w-4xl mx-auto text-center">
+        <p className="mb-6">
+          Became interested in coding through digital marketing and SEO
         </p>
-        <p className='mb-6'>
+        <p className="mb-6">
           Enjoys the challenge of creating complex UI logic
         </p>
-        <p className='mb-6'>Free time is spent around friends and family</p>
+        <p className="mb-6">Free time is spent around friends and family</p>
 
-        <div className='mx-auto lg:mx-0 w-fit min-h-[392px] md:min-h-[412px]'>
-          <span className='text-gray-400 text-sm md:text-lg mt-16 md:mt-20 mb-4 block text-left'>
+        <div className="mx-auto lg:mx-0 w-fit min-h-[392px] md:min-h-[412px]">
+          <span className="text-gray-400 text-sm md:text-lg mt-16 md:mt-20 mb-4 block text-left">
             Technology skills
           </span>
           <Skills
@@ -30,7 +30,7 @@ function Bio() {
           <Button
             showMore={showMore}
             handleClick={() => setShowMore(!showMore)}
-            variant='sm'
+            variant="sm"
           />
         </div>
       </div>
